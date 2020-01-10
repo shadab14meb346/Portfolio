8aa5f7c33a53e7c9580be5aeec7963b65365cbaf
Shadab <shadab14meb346@gmail.com>
2020-01-10 18:51:09 +0530

    updated for full stack information

--- src/components/about/aboutData.js
+++ src/components/about/aboutData.js
@@ -1,10 +1,11 @@
 let aboutData = {
-    "About_Me":"Dedicated, enthusiastic and a committed engineer with an on time and get it done attitude. Experience with various blockchain frameworks and web development technologies. Technical proficiencies include.Programming Languages: Javascript,SolidityFrameworks: Express, ReactTools: Truffle, GitMethodologies:OOPs,Functional ProgrammingCertificates: Blockchain Architecture Design and Use Cases, Ethereum and Solidity: The Complete Developer's Guide, The Web Developer Bootcamp ",
-    "Contact_Details":{
-        "Name":"Md Shadab Alam",
-        "Address":"Isthara Veera Hyderabad",
-        "Mobile":"+918218254094",
-        "Email":"shadab14meb346@gmail.com"
-    }
-}
+	About_Me:
+		"Dedicated, enthusiastic and committed engineer with an on time and get it done attitude.Experience with full - stack web development using MERN stack. ",
+	Contact_Details: {
+		Name: "Md Shadab Alam",
+		Address: "Isthara Veera Hyderabad",
+		Mobile: "+918218254094",
+		Email: "shadab14meb346@gmail.com"
+	}
+};
 export default aboutData;
