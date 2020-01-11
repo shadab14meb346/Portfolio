784d9aecbc3a7c0fb1524ce193c06e8e3a4dc0a8
Shadab <shadab14meb346@gmail.com>
2020-01-11 14:27:01 +0530

    updated skills

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -97,36 +97,43 @@ export default class Header extends Component {
 							</h1>
 						</div>
 						<div className="nine columns main-col">
-							{/* <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-              eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-              voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-              voluptatem sequi nesciunt.
-            </p> */}
 							<div className="bars">
 								<ul className="skills">
 									<li>
-										<span className="bar-expand photoshop" />
+										<span className="bar-expand css" />
 										<em>Node.js</em>
 									</li>
 									<li>
-										<span className="bar-expand illustrator" />
-										<em>Express</em>
-									</li>
-									<li>
-										<span className="bar-expand wordpress" />
-										<em>Solidity</em>
-									</li>
-									<li>
 										<span className="bar-expand css" />
-										<em>Docker</em>
+										<em>Express.JS</em>
 									</li>
 									<li>
 										<span className="bar-expand html5" />
-										<em>Blockchain</em>
+										<em>React.JS</em>
 									</li>
 									<li>
-										<span className="bar-expand jquery" />
-										<em>React</em>
+										<span className="bar-expand css" />
+										<em>MongoDB</em>
+									</li>
+									<li>
+										<span className="bar-expand css" />
+										<em>JavaScript</em>
+									</li>
+									<li>
+										<span className="bar-expand css" />
+										<em>REST</em>
+									</li>
+									<li>
+										<span className="bar-expand css" />
+										<em>GIT</em>
+									</li>
+									<li>
+										<span className="bar-expand html5" />
+										<em>HTML5</em>
+									</li>
+									<li>
+										<span className="bar-expand html5" />
+										<em>CSS3</em>
 									</li>
 								</ul>
 							</div>
